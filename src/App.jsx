9b306ab315dc7f3be0335c0d66af93ab76cbdcf2
Shivam <shivambhalla1499@gmail.com>
2020-12-26 19:53:56 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import Card from './Card';
+import './index.css';
+function App()
+{
+    return(
+        
+<>
+<h1 >NETFLIX</h1>
+<Card 
+imgsrc="https://source.unsplash.com/600x400/?devil"
+sname="DEVIL"
+title = "A Netflix Orignal series"
+link="https://www.netflix.com/in/"
+/>
+
+
+
+<Card 
+imgsrc="https://source.unsplash.com/600x400/?smoke"
+sname="NAMEKIAN"
+title = "A Netflix Orignal series"
+link="https://www.netflix.com/in/"
+/>
+
+
+<Card 
+imgsrc="https://source.unsplash.com/600x400/?smoke"
+sname="Beast"
+title = "A Netflix Orignal series"
+link="https://www.netflix.com/in/"
+/>
+
+<Card 
+imgsrc="https://source.unsplash.com/600x400/?smoke"
+sname="Immortal"
+title = "A Netflix Orignal series"
+link="https://www.netflix.com/in/"
+/>
+
+
+
+</>
+    )
+}
+
+export default App;
---
+++ src/Card.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Card (props)
+{
+  return (    
+    <>
+    
+    <div className = "cards" >
+      <div className="card">
+        <img src={props.imgsrc} alt ="myPic" className="card__img"/>
+          <div className="card__igit pushnfo">
+          <h3 className="card__title">{props.sname}</h3>
+            <span className="card__category">{props.title}</span>
+            <a href={props.link} target="_blank">
+            <button >
+            Watch Now
+            </button>
+            </a>
+          </div>
+      </div>
+    </div>
+    </>
+
+  )
+}
+
+export default Card;
