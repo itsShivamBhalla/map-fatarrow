090845e740445969a302dd44604e4b457c6bfb6a
Shivam <shivambhalla1499@gmail.com>
2020-12-29 15:16:34 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,43 +2,34 @@ import React from 'react';
 import Card from './Card';
 import cardData from './CardData';
 import './index.css';
+
+
 function App()
 {
     return(
         
 <>
 <h1 >NETFLIX</h1>
-<Card 
-imgsrc={cardData[0].imgsrc}
-sname={cardData[0].sname}
-title = {cardData[0].title}
-link={cardData[0].link}
-/>
+{cardData.map((val) => {
 
-<Card 
-imgsrc={cardData[1].imgsrc}
-sname={cardData[1].sname}
-title = {cardData[1].title}
-link={cardData[1].link}
-/>
-
-<Card 
-imgsrc={cardData[2].imgsrc}
-sname={cardData[2].sname}
-title = {cardData[2].title}
-link={cardData[2].link}
-/>
-
-<Card 
-imgsrc={cardData[3].imgsrc}
-sname={cardData[3].sname}
-title = {cardData[3].title}
-link={cardData[3].link}
-/>
-
-
-</>
+    return (
+        <Card 
+        imgsrc={val.imgsrc}
+        sname={val.sname}
+        title = {val.title}
+        link={val.link}
+        />
     )
+})}
+</>
+)
 }
 
 export default App;
+
+//    function sum(a,b){
+//       return a+b ;
+//   }
+//   now this with fat arrow function 
+//      const sum = (a,b)=> a+b;
+//
